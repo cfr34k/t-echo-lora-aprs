56ebeb63809f74d042f873c81138c1b37920d5f6
Thomas Kolb <cfr34k-git@tkolb.de>
2025-03-02 22:46:04 +0100

    techo_client: allow to tune modulation parameters

--- tools/ble_client/settings.py
+++ tools/ble_client/settings.py
@@ -12,6 +12,7 @@ SETTINGS_IDS = {
         'APRS_FLAGS':        0x0005,
         'LAST_BLE_SYMBOL':   0x0006,
         'LORA_RF_FREQUENCY': 0x0007,
+        'LORA_MOD_PARAMS':   0x0008,
     }
 
 APRS_FLAGS = {
@@ -23,6 +24,42 @@ APRS_FLAGS = {
         'ADD_WEATHER':       1 << 5,
         'STARTUP_RX_ON':     1 << 6,
         'STARTUP_TX_ON':     1 << 7,
+    }
+
+MOD_PARAMS_SF = {
+        0x05: 'SF 5',
+        0x06: 'SF 6',
+        0x07: 'SF 7',
+        0x08: 'SF 8',
+        0x09: 'SF 9',
+        0x0A: 'SF 10',
+        0x0B: 'SF 11',
+        0x0C: 'SF 12',
+    }
+
+MOD_PARAMS_CR = {
+        0x01: 'CR 4/5',
+        0x02: 'CR 4/6',
+        0x03: 'CR 4/7',
+        0x04: 'CR 4/8',
+    }
+
+MOD_PARAMS_BW = {
+        0x00: '7.81 kHz',
+        0x08: '10.42 kHz',
+        0x01: '15.63 kHz',
+        0x09: '20.83 kHz',
+        0x02: '31.25 kHz',
+        0x0A: '41.67 kHz',
+        0x03: '62.50 kHz',
+        0x04: '125 kHz',
+        0x05: '250 kHz',
+        0x06: '500 kHz',
+    }
+
+MOD_PARAMS_LDRO = {
+        0x00: 'OFF',
+        0x01: 'ON',
     }
 
 LORA_POWERS_DBM = [22, 20, 17, 14, 10, 0, -9]
@@ -40,7 +77,7 @@ class Setting:
 
     def format(self):
         if self.data is None:
-            return '[None]'
+            return '[None/Default]'
 
         if self.name in ['SOURCE_CALL', 'COMMENT', 'SYMBOL_CODE', 'LAST_BLE_SYMBOL']:
             null_idx = self.data.index(0)
@@ -54,6 +91,13 @@ class Setting:
         elif self.name == 'LORA_RF_FREQUENCY':
             freq, = struct.unpack('<I', self.data)
             return f"{freq} Hz"
+        elif self.name == 'LORA_MOD_PARAMS':
+            sf, bw, cr, ldro = struct.unpack('4B', self.data)
+            sf_s = MOD_PARAMS_SF[sf]
+            bw_s = MOD_PARAMS_BW[bw]
+            cr_s = MOD_PARAMS_CR[cr]
+            ldro_s = MOD_PARAMS_LDRO[ldro]
+            return f"{sf_s}, {bw_s}, {cr_s}, {ldro_s}"
         else:
             return f"{self.data}"
 
@@ -155,6 +199,54 @@ class Setting:
             self.data = struct.pack("<I", freq)
             return True
 
+    def _edit_mod_params(self):
+        if not self.data:
+            print("\n!!!")
+            print("WARNING: modulation parameters are not set yet on the device. Assuming default values.")
+            print("!!!\n")
+            local_data = [0x0C, 0x04, 0x01, 0x01]
+        else:
+            local_data = list(self.data)
+
+        modified = False
+
+        while True:
+            options = {
+                    '0': f"Spreading Factor [{MOD_PARAMS_SF[local_data[0]]}]",
+                    '1': f"Bandwidth [{MOD_PARAMS_BW[local_data[1]]}]",
+                    '2': f"Coding Rate [{MOD_PARAMS_CR[local_data[2]]}]",
+                    '3': f"Low Data Rate Optimization [{MOD_PARAMS_LDRO[local_data[3]]}]",
+                }
+
+            selected = menu.choose_option("Select the parameter to edit:", options)
+
+            if not selected:
+                self.data = bytes(local_data)
+                return modified
+            elif selected == '0':
+                options = dict(zip(range(len(MOD_PARAMS_SF)), MOD_PARAMS_SF.values()))
+                selected_sf = menu.choose_option("Select the new spreading factor:", options)
+                if selected_sf != 'q':
+                    local_data[0] = list(MOD_PARAMS_SF.keys())[int(selected_sf)]
+                    modified = True
+            elif selected == '1':
+                options = dict(zip(range(len(MOD_PARAMS_BW)), MOD_PARAMS_BW.values()))
+                selected_bw = menu.choose_option("Select the new bandwidth:", options)
+                if selected_bw != 'q':
+                    local_data[1] = list(MOD_PARAMS_BW.keys())[int(selected_bw)]
+                    modified = True
+            elif selected == '2':
+                options = dict(zip(range(len(MOD_PARAMS_CR)), MOD_PARAMS_CR.values()))
+                selected_cr = menu.choose_option("Select the new coding rate:", options)
+                if selected_cr != 'q':
+                    local_data[2] = list(MOD_PARAMS_CR.keys())[int(selected_cr)]
+                    modified = True
+            elif selected == '3':
+                options = dict(zip(range(len(MOD_PARAMS_LDRO)), MOD_PARAMS_LDRO.values()))
+                selected_ldro = menu.choose_option("Enable Low Data Rate Optimization?", options)
+                if selected_ldro != 'q':
+                    local_data[3] = list(MOD_PARAMS_LDRO.keys())[int(selected_ldro)]
+                    modified = True
 
     def edit_interactive(self):
         modified = False
@@ -169,6 +261,8 @@ class Setting:
             modified = self._edit_flags()
         elif self.name == 'LORA_RF_FREQUENCY':
             modified = self._edit_rf_frequency()
+        elif self.name == 'LORA_MOD_PARAMS':
+            modified = self._edit_mod_params()
 
         if modified:
             self.modified = True
